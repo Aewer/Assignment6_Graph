174de7ae045d1de0161e255d67c6fed537aae1ae
aewer <slawa.miroshnik.2004@gmail.com>
2023-05-31 23:19:00 +0600

    addVertex

--- src/MyGraph.java
+++ src/MyGraph.java
@@ -4,4 +4,7 @@ import java.util.Map;
 
 public class MyGraph<V> {
     private Map<Vertex<V>, List<Vertex<V>>> map = new HashMap<>();
+    public void addVertex(V data) {
+        Vertex<V> vertex = new Vertex<>(data);
+    }
 }
